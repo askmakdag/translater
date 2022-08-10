4252b1fee82056128ba03035af379d73fd89a8d1
askimakdag <Askmakdag2020>
2022-08-10 22:09:17 +0300

    header component was added

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,27 +1,49 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 
-export default function Header() {
-  const style = styles();
+interface HeaderProps {
+  left?: JSX.Element;
+  title?: string;
+  right?: JSX.Element;
+}
+
+export default function Header({
+  left,
+  title = 'Translater',
+  right,
+}: HeaderProps) {
   return (
     <View style={style.container}>
-      <Text style={style.title}>Translater</Text>
+      <View style={style.left}>{left}</View>
+
+      <Text style={style.title}>{title}</Text>
+
+      <View style={style.right}>{right}</View>
     </View>
   );
 }
 
-const styles = () =>
-  StyleSheet.create({
-    container: {
-      flexDirection: 'row',
-      justifyContent: 'center',
-      alignItems: 'center',
-      height: 45,
-      width: '100%',
-      backgroundColor: '#1973E7',
-    },
-    title: {
-      color: 'white',
-      fontSize: 17,
-    },
-  });
+const style = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: 45,
+    width: '100%',
+    paddingHorizontal: 8,
+    backgroundColor: '#1973E7',
+  },
+  left: {
+    flex: 1,
+  },
+  title: {
+    flex: 1,
+    color: 'white',
+    fontSize: 17,
+    textAlign: 'center',
+  },
+  right: {
+    flex: 1,
+    alignItems: 'flex-end',
+  },
+});
